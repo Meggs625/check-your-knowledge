594ae418f7474935f280422c1c3463355360dbdb
Megan McBride <megan.mcbride@colorado.edu>
2021-07-05 18:41:45 -0600

    Create the Turn test file and cooresponding class without any edge cases

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -20,5 +20,32 @@ describe('Turn', function() {
     expect(turn.card).to.equal(card);
   });
 
+  it('should provide the user\'s guess', () => {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('array', card);
 
-})
+    expect(turn.returnGuess()).to.equal('array');    
+  });
+
+  it('should provide the current card information', () => {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('array', card);
+
+    expect(turn.returnCard()).to.equal(card);
+  });
+
+  it('should evaluate if the user\'s guess is the correct answer', () => {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('array', card);
+
+    expect(turn.evaluateGuess()).to.equal(false);
+  });
+
+  it('should provide feedback on whether the guess was correct or not', () => {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('array', card);
+
+    expect(turn.giveFeedback()).to.equal('incorrect!');
+  });
+
+});
