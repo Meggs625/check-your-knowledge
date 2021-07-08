e646b9a3648d5f1e8dae1769dd59520816e200e3
Megan McBride <megan.mcbride@colorado.edu>
2021-07-07 19:13:58 -0600

    Clean the code and improve tests

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -9,23 +9,29 @@ describe('Deck', () => {
   let card1;
   let card2;
   let card3;
+  let deck; 
 
   beforeEach(() => {
     card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
     card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    deck = new Deck([card1, card2, card3]);
   })
-  
+
+  it('should be a function', () => {
+    expect(Deck).to.be.a('function');
+  })
+
   it('should accept a new card array', () => {
     const deck = new Deck([card1, card2])
     expect(deck.cards.length).to.equal(2);  
   })
+
   it('should accept more cards', () => {
-    const deck = new Deck([card1, card2, card3]);
     expect(deck.cards.length).to.equal(3);  
   })
+  
   it('should return the total card count in the deck', () => {
-    const deck = new Deck([card1, card2, card3]);
     expect(deck.countCards()).to.equal(3);
   })
     
--- test/Game-test.js
+++ test/Game-test.js
@@ -1,10 +1,7 @@
 const chai = require('chai');
 const expect = chai.expect;
-const data = require('../src/data');
-const prototypeQuestions = data.prototypeData;
 
 const Deck = require('../src/Deck');
-const Card = require('../src/Card');
 const Round = require('../src/Round');
 const Game = require('../src/Game');
 
@@ -26,13 +23,16 @@ describe('Game', function() {
 
   it('should create a new array of 10 cards', () => {
     game.start();
-    expect(game.createCards()).to.be.an('array');
-    expect(game.createCards().length).to.equal(10);
+    let card = game.createCards();
+    let cardLength = game.createCards().length;
+    expect(card).to.be.an('array');
+    expect(cardLength).to.equal(10);
   })
 
   it('should create a deck of cards', () => {
     game.start();
-    expect(game.createDeck()).to.be.instanceOf(Deck);
+    let newDeck = game.createDeck(); 
+    expect(newDeck).to.be.instanceOf(Deck);
   })
 
   it('should create a new current round with the new deck', () => {
